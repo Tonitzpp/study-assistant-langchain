2cf0a8e8dafc6a50130c1125d8cd3507f1eddb9d
Tonitzpp <seu_email@gmail.com>
2026-08-27 16:43:20 -0300

    add gitkeep and initial imports

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+import os
+import glob
+
+
